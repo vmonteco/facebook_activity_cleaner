7050655050a7137a5e4d8bd92142a4a67cd081dd
vincent montecot <vmonteco@student.42.fr>
2015-09-06 04:13:41 +0200

    console.log() calls removed

--- Facebook_cleaner.user.js
+++ Facebook_cleaner.user.js
@@ -20,8 +20,6 @@
 ** Variables.
 */
 
-console.log("test0");
-
 // Texts :
 var confirm_text = "Do you want to delete everything?";
 
@@ -37,8 +35,6 @@ var activity_selector = 'div[class="pam _5shk uiBoxWhite bottomborder"]';
 var delete_button_selector = 'a[class="_54nc"]';
 var confirm_selector = 'button[class="_42ft _4jy0 layerConfirm uiOverlayButton _4jy3 _4jy1 selected _51sy"]';
 
-console.log("test1");
-
 /*
 ** Basic functions.
 */
@@ -46,20 +42,13 @@ console.log("test1");
 function check_timeline()
 {
 	// NB : It only works if the URL matches exactly "/allactivity". "/allactivity#" didn't work.
-	console.log("test1-1");
 	if (/(allactivity)/g.test($(location).attr('href'))){
-		console.log("test1-2");
 		return true;
 	}
-	console.log("test1-3");
 	return false;
 }
 
-console.log("test1bis");
-
-function add_button(text, code){
-	console.log("test1bis1");
-	
+function add_button(text, code){	
 	// Creating the button.
 	var button = document.createElement('button');
 	
@@ -83,53 +72,67 @@ function add_button(text, code){
 
 	// make the location append the button.
 	location.append(button);
-
-	console.log("test1bis-2");
 }
 
-console.log("test1ter");
-
 function ask_for_confirmation(){
-	console.log("test1ter1");
 	return confirm(confirm_text);
 }
 
-console.log("test2");
-
-function delete_activity(elem){
-	console.log("test2-0");
-	console.log(elem);
-	console.log(activity_button);
-	console.log($(document).find);
-	console.log(elem.find);
-	console.log(elem[0]);
-	console.log(elem.find(activity_button));
-	var button = elem[0].find(activity_button);
-	console.log("test2-1");
-	click_on_elem(button);
-	console.log("test2-2");
-	var delete_button = get_delete_button($(document).find(delete_button_selector));
-	console.log("test2-3");
-	if (delete_button){
-		console.log("test2-4");
-		if (test){
-			console.log("test2-5");
-			console.log('test : {0}'.format(elem));
-			console.log("test2-6");
-		}
-		else{
-			console.log("test2-7");
-			click_on_elem(delete_button);
-			console.log("test2-8");
-			// confirm
-			console.log("test2-9");
-			click_on_elem($(document).find(confirm_selector));
-			console.log("test2-10");
-		}
-	}
+function get_button_with_word(word){
+	var selector = 'a:contains("' + word + '")';
+	var button =  $(document).find(selector);
+	return button;
 }
 
-console.log("test3");
+function get_delete_button()
+{
+	var commands = [
+		'Delete',
+		'Hidden from Timeline'
+	];
+	for (word in commands){
+		var button = get_button_with_word(word);
+		if (button){
+			return button;
+		}
+	}
+	return null;
+}
+
+// function delete_activity(elem){
+// 	var button = $(elem).find(activity_button);
+// 	click_on_elem(button);
+// 	var delete_button = get_delete_button($(document).find(delete_button_selector));
+// 	if (delete_button){
+// 		if (test){
+// 			console.log('test : ' + elem);
+// 		}
+// 		else{
+// 			click_on_elem(delete_button);
+// 			// confirm
+// 			click_on_elem($(document).find(confirm_selector));
+// 		}
+// 	}
+// }
+
+function delete_activity(elem){
+	console.log("test 1-1");
+	var button = $(elem).find(activity_button);
+	console.log("test 1-2");
+	click_on_elem(button);
+	console.log("test 1-3");
+	var delete_button = get_delete_button($(document).find(delete_button_selector));
+	console.log("test 1-4");
+	if (test){
+		console.log("test 1-5");
+		console.log('test : ' + elem);
+	}
+	else if (delete_button){
+			click_on_elem(delete_button);
+			// confirm
+			click_on_elem($(document).find(confirm_selector));
+	}
+}
 
 function click_event(){
 	return new MouseEvent('click', {
@@ -139,146 +142,99 @@ function click_event(){
 	});
 }
 
-console.log("test3-1");
-
 function scroll(){
 	return scrollTo(0, 1000000000);
 }
 
-console.log("test4");
-
 function get_activities(){
-	console.log("test4-1");
 	var activities = $(document).find(activity_selector);
-	console.log("test4-2");
-	console.log(activities);
 //	scroll();
 	return activities;
 }
 
-console.log("test4bis");
-
 function click_on_elem(elem){
 	elem[0].dispatchEvent(click_event());
 }
-
-console.log("test5");
 
 /*
 ** Evolved functions.
 */
 
 function delete_all_activity(){
-	console.log("test5-1");
 	var activities = [];
 	var fn = function(index, value){
-		console.log("test5-2");
 		console.log("Delete no " + index + " - object : " + value);
+		console.log("test 1");
 		delete_activity(value);
-		console.log("test5-3");
-		this.remove();
-		console.log("test5-4");
+		console.log("test 2");
+		$(value).remove();
+		console.log("test 3");
 	};
 	if (ask_for_confirmation()){
-		console.log("test5-5");
 		while (activities = get_activities()){
-			console.log("test5-6");
+			console.log(activities);
 			activities.each(fn);
-			console.log("test5-7");
 			activities = [];
 		}
 	}
 	return true;
 }
 
-console.log("test6");
-
 function add_all_button(){
-	console.log("test6-1");
 	var text = 'Delete ALL this shit!';
 	var fn = function(){
 		delete_all_activity();
 	};
 	add_button(text, fn);
-	console.log("test6-2");
 }
 
-console.log("test7");
-
 function add_one_button(){
-	console.log("test7-1");
 	var text = 'Delete ONE of this shit!';
 	var fn = function(){
 		alert('Delete one');
 	};
 	add_button(text, fn);
-	console.log("test7-2");
 }
-
-console.log("test8");
 
 // main().
 
 function main(){
-	console.log("test8-1");
 	console.log("launched state : " + launched);
 	if (!launched){
-		console.log("test8-2");
 		launched = true;
-		console.log("test8-3");
 		add_all_button();
-		console.log("test8-4");
 		add_one_button();
-		console.log("test8-5");
 	}
 }
 
-console.log("test9");
-
 function detect_node_for_buttons(mutations){
-	console.log("test9-1");
 	mutations.forEach(function (mutation){
-		console.log("test9-2");
 		var element = $(document).find(button_location);
 		if (element){
-			console.log("test9-3");
 			main();
-			console.log("test9-4");
 			observer.disconnect();
-			console.log("test9-5");
 			return;
 		}
-		console.log("test9-6");
 		if (!mutation.addedNodes) return;
-		console.log("test9-7");
 		for (var i = 0; i < mutation.addedNodes.length; i++){
-			console.log("test9-8");
 			if (mutation.addedNodes[i].matches(button_location)){
-				console.log("test9-9");
 				main();
-				console.log("test9-10");
 				observer.disconnect();
-				console.log("test9-11");
 				return;
 			}
 		}
 	});
 }
 
-console.log("test10");
-
 /*
 ** MutationObserver.
 */
 
 if (check_timeline()){
-	console.log("test10-1");
 	var observer = new MutationObserver(function (mutations){
 		detect_node_for_buttons(mutations);
 	});
 }
-
-console.log("test11");
 
 observer.observe(document.body, {
     childList: true,
@@ -286,5 +242,3 @@ observer.observe(document.body, {
     attributes: false,
     characterData: false,
 });
-
-console.log("test12");
